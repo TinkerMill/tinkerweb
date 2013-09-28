ef31fef42ccbab7b255aee55812813838dbb7b15
Cody B. Daig <cody@daig.me>
2013-09-28 17:27:48 -0600

    Create a Sample Config File

---
+++ globals/config/config_sample.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * The purpose of this file is to define all secure passwords, api keys, etc...
+ * 
+ * This is an example config file. For this file to work, please rename it to config.php!!
+ * 
+ * @author Cody B. Daig
+ * 
+ * Last Modified: Sept. 28, 2013
+ */
+?>
