4c0a24789182e2978ae9d8af44d427bf38ebcec7
Cody B. Daig <cody@daig.me>
2013-09-28 17:20:08 -0600

    Set-Up Base File Structure
    (does not include the "features" and "globals" directories)

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This is the index.php file
+ * This file will be the first file served,
+ *      the .htaccess file will also force all incoming requests through this file
+ * 
+ * @author Cody B. Daig
+ * 
+ * Last Modified: September 28th, 2013
+ */
+?>
