71747a28ba4fd52250fd3d0d17b12ec3072edd82
Cody B. Daig <cody@daig.me>
2013-09-28 20:21:05 -0600

    Set-Up Site Title Variable

--- globals/config/vars.php
+++ globals/config/vars.php
@@ -2,9 +2,14 @@
 
 /*
  * The purpose of this file is to define the site-wide variables.
+ * Any variables in this file should NOT contain any secure information.
+ *     (All secure info should be in config.php)
  * 
  * @author Cody B. Daig
  * 
  * Last Modified: Sept. 28, 2013
  */
+
+$SITETITLE = "Tinkermill";  // <title> Information will be displayed as "PageTitle - $SITETITLE"
+
 ?>
