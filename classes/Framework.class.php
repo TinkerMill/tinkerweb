0625c1457a26f48aa26c722ddafd8f7197035ba1
Cody B. Daig <cody@daig.me>
2013-10-02 22:40:51 -0600

    Added Header Comment to Framework Class

--- classes/Framework.class.php
+++ classes/Framework.class.php
@@ -1,14 +1,32 @@
 <?php
 
+/*
+ * This is the Framework Class
+ * This class will be the core functionality of the entire website
+ * To include loading the site layout.
+ * 
+ * @author Cody B. Daig
+ * 
+ * Last Modified: October 2nd, 2013 
+ */
+
 class Framework {
 
   // __construct
   // Loads the Core Framework
   public function __construct() {
   }
+  
+  public function loadHeader(){
+    include_once('../globals/layout/header.php');
+  }
+  
+  public function loadFooter(){
+    include_once('../globals/layout/footer.php');
+  }
 
   public function rootURL() {
-    if ($SSL == true) {
+    if (SSL == true) {
       $URL = "https://";
     } else {
       $URL = "http://";
