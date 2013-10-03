174ce98163b611e1e44438600539563ef40e3042
Cody B. Daig <cody@daig.me>
2013-10-02 22:53:09 -0600

    Created footer.php

---
+++ globals/layout/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * header.php file
+ * This file will load the page until page specific content starts
+ * 
+ * @author Cody B. Daig
+ * 
+ * Last Modified: October 2nd, 2013 
+ */
+
+?>
