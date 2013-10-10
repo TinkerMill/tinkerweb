4453cd779fbde98dc29d78df7fe9e96aca129bf4
Cody B. Daig <cody@daig.me>
2013-10-09 20:23:48 -0600

    Set-Up the Blog Class

--- classes/Blog.class.php
+++ classes/Blog.class.php
@@ -6,7 +6,32 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 2nd, 2013 
+ * Last Modified: October 9th, 2013 
  */
+
+// Include the Parent Framework Class
+require_once("Framework.class.php");
+
+class Blog extends Framework{
+  public function __construct(){
+    // Load the Website's Header
+    parent::loadHeader();
+    
+    // Determine what is being requested
+    if(URL == "/blog/")
+    {
+      // Load the Default Blog Page
+      $this->listPosts();
+    }
+    
+    // Load the Footer
+    parent::loadFooter();
+  }
+  
+  public function listPosts(){
+    // Displays the List Events View
+    echo "This is where blog posts will be listed.";
+  }
+}
 
 ?>
