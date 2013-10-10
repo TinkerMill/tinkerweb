7f30d398c3e24995ae0a35e792d614617e4b1c11
Cody B. Daig <cody@daig.me>
2013-10-09 20:29:21 -0600

    Set-Up the Forum Class

--- classes/Forum.class.php
+++ classes/Forum.class.php
@@ -6,6 +6,32 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 2nd, 2013 
+ * Last Modified: October 9th, 2013 
  */
+
+// Include the Parent Framework Class
+require_once("Framework.class.php");
+
+class Forum extends Framework{
+  public function __construct(){
+    // Load the Website's Header
+    parent::loadHeader();
+    
+    // Determine what is being requested
+    if(URL == "/forum/")
+    {
+      // Load the Default Blog Page
+      $this->showForum();
+    }
+    
+    // Load the Footer
+    parent::loadFooter();
+  }
+  
+  public function showForum(){
+    // Displays the List Events View
+    echo "This is where the Forum will be displayed.";
+  }
+}
+
 ?>
