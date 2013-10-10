2e432a3c04cbe746c27f2d4181a50c344815c602
Cody B. Daig <cody@daig.me>
2013-10-09 21:00:33 -0600

    Set-Up Blog backend Class

--- classes/Blog.backend.class.php
+++ classes/Blog.backend.class.php
@@ -10,27 +10,19 @@
  */
 
 // Include the Parent Framework Class
-require_once("Framework.class.php");
+require_once("Backend.class.php");
 
-class Blog extends Framework{
+class BackendBlog extends Backend{
   public function __construct(){
-    // Load the Website's Header
-    parent::loadHeader();
     
-    // Determine what is being requested
-    if(URL == "/blog/")
-    {
       // Load the Default Blog Page
       $this->listPosts();
-    }
-    
-    // Load the Footer
-    parent::loadFooter();
+
   }
   
   public function listPosts(){
     // Displays the List Events View
-    echo "This is where blog posts will be listed.";
+    echo "This is where blog posts will be listed from the Backend.";
   }
 }
 
