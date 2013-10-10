b1f36279ffc9da6df83d4e705ccb1a4e3eb7d531
Cody B. Daig <cody@daig.me>
2013-10-09 20:28:13 -0600

    Set-Up the Equipment Class

--- classes/Equipment.class.php
+++ classes/Equipment.class.php
@@ -6,6 +6,32 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 2nd, 2013 
+ * Last Modified: October 9th, 2013 
  */
+
+// Include the Parent Framework Class
+require_once("Framework.class.php");
+
+class Equipment extends Framework{
+  public function __construct(){
+    // Load the Website's Header
+    parent::loadHeader();
+    
+    // Determine what is being requested
+    if(URL == "/equipment/")
+    {
+      // Load the Default Blog Page
+      $this->showEquipment();
+    }
+    
+    // Load the Footer
+    parent::loadFooter();
+  }
+  
+  public function showEquipment(){
+    // Displays the List Events View
+    echo "This is where the Equipment will be displayed.";
+  }
+}
+
 ?>
