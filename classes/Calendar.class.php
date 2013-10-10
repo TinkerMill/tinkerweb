1482c1f1b7cdeec7c77804104b414f767ec6f4dc
Cody B. Daig <cody@daig.me>
2013-10-09 20:24:54 -0600

    Set-Up the Calendar Class

--- classes/Calendar.class.php
+++ classes/Calendar.class.php
@@ -6,6 +6,32 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 2nd, 2013 
+ * Last Modified: October 9th, 2013 
  */
+
+// Include the Parent Framework Class
+require_once("Framework.class.php");
+
+class Calendar extends Framework{
+  public function __construct(){
+    // Load the Website's Header
+    parent::loadHeader();
+    
+    // Determine what is being requested
+    if(URL == "/calendar/")
+    {
+      // Load the Default Blog Page
+      $this->showCalendar();
+    }
+    
+    // Load the Footer
+    parent::loadFooter();
+  }
+  
+  public function showCalendar(){
+    // Displays the List Events View
+    echo "This is where the Calendar will be displayed.";
+  }
+}
+
 ?>
