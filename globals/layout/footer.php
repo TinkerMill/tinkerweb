601a7cf38113454a4112fb169ef5c8774b599ada
Cody B. Daig <cody@daig.me>
2013-10-10 14:19:50 -0600

    Added Bootstrap to Header and Footer

--- globals/layout/footer.php
+++ globals/layout/footer.php
@@ -6,10 +6,23 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 9th, 2013 
+ * Last Modified: October 10th, 2013 
  */
 
 ?>
+</div>
+
+<div id="footer">
+      <div class="container">
+        <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+      </div>
+    </div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://dev.tinkermill.org/bootstrap/assets/js/jquery.js"></script>
+    <script src="http://dev.tinkermill.org/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
 
--- globals/layout/header.php
+++ globals/layout/header.php
@@ -6,21 +6,73 @@
  * 
  * @author Cody B. Daig
  * 
- * Last Modified: October 9th, 2013 
+ * Last Modified: October 10th, 2013 
  */
 
 ?>
 
 <!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-  
+<html lang="en">
   <head>
-    
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
     
     <title><?php echo SITETITLE; ?></title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="http://dev.tinkermill.org/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://dev.tinkermill.org/css/site.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
     
   </head>
   
   <body>
+      
+          <!-- Wrap all page content here -->
+    <div id="wrap">
+
+      <!-- Fixed navbar -->
+      <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+
+      <!-- Begin page content -->
+      <div class="container">
