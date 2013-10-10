69b8b839d384f2bd6232eccf44801e061923d421
Cody B. Daig <cody@daig.me>
2013-10-09 20:31:18 -0600

    Set-Up the Wiki Class

--- classes/Wiki.class.php
+++ classes/Wiki.class.php
@@ -8,4 +8,30 @@
  * 
  * Last Modified: October 2nd, 2013 
  */
+
+// Include the Parent Framework Class
+require_once("Framework.class.php");
+
+class Wiki extends Framework{
+  public function __construct(){
+    // Load the Website's Header
+    parent::loadHeader();
+    
+    // Determine what is being requested
+    if(URL == "/wiki/")
+    {
+      // Load the Default Blog Page
+      $this->showWiki();
+    }
+    
+    // Load the Footer
+    parent::loadFooter();
+  }
+  
+  public function showWiki(){
+    // Displays the List Events View
+    echo "This is where the Wiki will be displayed.";
+  }
+}
+
 ?>
