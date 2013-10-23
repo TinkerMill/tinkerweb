450a62e37018a245d11ee9c289b2dd0fa413eb5a
Cody B. Daig <cody@daig.me>
2013-10-23 00:17:27 -0600

    Quick Blog Class as test class

---
+++ features/blog/blog.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Blog{
+    public function __construct(){
+        echo "<h1>Welcome to the Blog</h1>";
+    }
+    public function listPosts(){
+        echo "<p>This will list the posts</p>";
+    }
+}
