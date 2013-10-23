4cbe476438594ff63ec56decf5bb18c1020e10e6
Cody B. Daig <cody@daig.me>
2013-10-22 19:08:47 -0600

    Started the index.php file

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php/* 
+
+
+ * index.php
+ * 
+ * This is the heart of the website. Every page request (that is not an actual file on the server) 
+ * starts here. This page is responsible for determing what is being requested, permissions, and how to load it.
+ * 
+ * Author: @codydaig 
+ * 
+ * Last Modified: October 22, 2013
+ */
+
+
+
+?>
