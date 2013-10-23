ab4733f20f132379061b81cf0bb65bf09d198224
Cody B. Daig <cody@daig.me>
2013-10-23 01:00:19 -0600

    Formatting

--- public/index.php
+++ public/index.php
@@ -30,44 +30,48 @@ if ($_SERVER["REDIRECT_URL"] != "") {
 }
 
 // See if the Page exists in the Database
-$sql = "SELECT * FROM `pages` WHERE `URL`='" . $url . "'";
+$sql = "SELECT `pages`.*, `template`.* FROM `pages` INNER JOIN `template` ON `pages`.`Template`=`template`.`ID` WHERE `URL`='" . $url . "'";
 $result = mysqli_query($con, $sql);
 if ($mysqli->connect_errno) {
     // Something Happened and we could not query the database properly
     echo "Failed to Query the Database. Please try again later, or contact webmaster@tinkermill.org";
     die();
-} else if (mysqli_num_rows($result)>0) {
+} else if (mysqli_num_rows($result) > 0) {
     $page = mysqli_fetch_array($result);
-    
+
     $pID = $page["pID"];
     $hash = "hash1234";
-    if($pID != 0){
+    if ($pID != 0) {
         $sql = "SELECT * FROM users";
-        if($pID > 0){
+        if ($pID > 0) {
             $sql .= " INNER JOIN permissions_access ON users.ID=permissions_access.uID";
         }
         $sql .= " WHERE users.Hash='" . $hash . "'";
-        if($pID > 0){
+        if ($pID > 0) {
             $sql .= " AND permissions_access.pID=$pID";
         }
         $permission = mysqli_query($con, $sql);
-        if(mysqli_num_rows($permission)<=0){
+        if (mysqli_num_rows($permission) <= 0) {
             // Permission Denied
             die("Permission Denied");
         }
     }
-    
+
     // Include the Header
-    
-    // Require the Class File
-    $classFile = "../" . $page["ClassFile"];
-    require_once($classFile);
-    // Initiate the Class
-    $class = new $page["ClassName"]();
-    // Call the Page Function
-    $class->$page["Function"]();
-    
+    $headerFile = "../" . $page["headerFile"];
+    include($headerFile);
+
+        // Require the Class File
+        $classFile = "../" . $page["ClassFile"];
+        require_once($classFile);
+        // Initiate the Class
+        $class = new $page["ClassName"]();
+        // Call the Page Function
+        $class->$page["Function"]();
+
     // Include the Footer
+    $footerFile = "../" . $page["footerFile"];
+    include($footerFile);
 } else {
     // No Page Was Found, Load the 404 page
 }
