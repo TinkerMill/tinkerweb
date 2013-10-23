0672e390bee13026a7710431d28f190cfeda799c
Cody B. Daig <cody@daig.me>
2013-10-22 19:39:47 -0600

    The config.sample.php file

---
+++ globals/config/config.sample.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * The purpose of this file is to define all secure passwords, api keys, etc...
+ * 
+ * This is an example config file. For this file to work, please rename it to config.php!!
+ * 
+ * @author Cody B. Daig
+ * 
+ * Last Modified: October 22nd, 2013
+ */
+
+
+// ---------- MYSQL CONNECTION INFORMATION  ----------
+$SQLHOST = "localhost";     // mySQL Host (usually localhost)
+$SQLUSER = "user";      // mySQL Username
+$SQLPASS = "pass";      // mySQL Password
+$SQLDB = "dbname";          // mySQL Database Name
+
+?>
